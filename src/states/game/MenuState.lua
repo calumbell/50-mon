676360f01d598c6cc8224ef2b4eb9b16ce43b15d
Calum Bell <calumabell@googlemail.com>
2020-08-08 20:07:43 +0100

    created basic MenuState class

--- src/states/game/MenuState.lua
+++ src/states/game/MenuState.lua
@@ -8,14 +8,41 @@
 
 MenuState = Class{__includes = BaseState}
 
-function MenuState:init()
+function MenuState:init(params, battleState)
+	self.battleState = params.battleState
 
+	if params.menuType == "level up" then
+		self.menu = Menu ({
+			x = VIRTUAL_WIDTH - 128,
+			y = 64,
+			width = 128,
+        	height = VIRTUAL_HEIGHT - 64,
+        	selectionEnabled = false,
+        	items = {
+        		{
+        			text = "HP"
+        		},
+
+        		{
+        			text = "Attack"
+        		},
+
+        		{
+        			text = "Defense"
+        		},
+
+        		{
+        			text = "Speed"
+        		}
+        	}
+		})
+	end
 end
 
 function MenuState:update(dt)
-
+	self.menu:update(dt)
 end
 
 function MenuState:render()
-
+	self.menu:render()
 end
